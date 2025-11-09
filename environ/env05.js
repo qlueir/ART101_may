0b8b147389e570da7bcb5fe0d5df0717e5e745ed
qlueir <qarqizhhh8@gmail.com>
2025-11-09 10:26:02 -0800

    fish sounds wip

--- environ/env05.js
+++ environ/env05.js
@@ -1,18 +1,3 @@
-/*
-$(".yes").click(function() {
-      var image = document.getElementById("cat");
-      image.src = "images/exploding_cat.gif";
-      $(".yes").html("Kitty has exploded!");
-});
-
-
-$(".no").click(function() {
-      var image = document.getElementById("guppy");
-      image.src = "images/Vert.webp";
-      $(".no").html("You've killed Guppy!");
-});
-*/
-
 $("button").click(function() {
 
     if ($(this).hasClass("yes")){
@@ -28,5 +13,21 @@ $("button").click(function() {
     }
 
 });
+
+var audio = new Audio ('yippee-tbh.mp3');
+var audio2 = new Audio ('')
+//create var for fish tank audio?^^
+
+$("#cat").hover(function(){
+
+      audio.play();
+//only play once per hover
+});
+
+$("html") audio2.play ('')
+//fish tank audio loop
+//play battle music when click on expand
+//stop battle music after player click yes/no button
+//continue fish tank audio
 
 
