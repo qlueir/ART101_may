9267994cf038a8a934fa70a11a9d8b1b94fe1445
qlueir <qarqizhhh8@gmail.com>
2025-11-24 16:21:23 -0800

    working lab 9

--- environ/env09.js
+++ environ/env09.js
@@ -1,62 +1,61 @@
 $(document).ready()
+
     //variables
     let OST = new Audio ('OST.mp3')
     OST.play('OST.mp3')
     OST.loop = true
+
     let munch = new Audio ('munch.mp3')
     let explosion = new Audio ('explosion.mp3')
     let ending = new Audio ('battle.mp3')
     let yippee = new Audio ('yippee-tbh.mp3');
 
-    //play battle music when click on expand + OST pause
-    $(".ending").click(function() {
+    //button goes to bothpages2 + battle music start + OST pause
+    $(".nextpage").click(function() {
         ending.play();
         OST.pause();
 
-        //gun image follows mouse
-        $(document).mousemove(function(event) {
-            $("#creature").css({
-            left: event.pageX - 60,
-            top: event.pageY - 60,
-            position: "absolute"
-    });
-
+        $(".ch1").hide();
+        $(".ch2").show();
 
     });
 
+    //gun image follows mouse
+    $(document).mousemove(function(event) {
+        $("#shotgun").css({
+        left: event.pageX - 60,
+        top: event.pageY - 60,
+        position: "absolute"
+        });
+    });
 
     //yes+no button click replaces with new text + image + audio
     $(".yes, .no").click(function() {
 
-        //stop battle.mp3
         ending.pause();
         ending.currentTime = 0;
-        ending.src = "";
         
         if ($(this).hasClass("yes")){
             $(".yes").html("Kitty has exploded!");
-            let image = document.getElementById("cat");
-            image.src = "images/exploding_cat.gif";
+            $("#catsmile").attr("src", "images/exploding_cat.gif");
             explosion.play();
-            }
-    
-        else {
+        } else {
             $(".no").html("You've killed Guppy!");
-            let image = document.getElementById("guppy");
-            image.src = "images/Vert.webp";
+            $("#vert").show();
             munch.play();
-            }
+        }
     });
 
-    //*clikc on peephole image transform into cat image and show text DOESNT WORK :(
-    /*
-    $(".peeping").click(function() {
+
+    //hover on peephole image transform into cat image and show text DOESNT WORK :(
+    
+    $(".peeping").hover(function() {
         $('#peephole').attr('src', 'images/cat_point5.jpg');
-        $(".showtext").show();
+        yippee.play();
+    }, function() {
+        $('#peephole').attr('src', 'images/peephole.webp');
     });
-    */
-
-});
+    
 
     //guppy bounce animation
     const image = document.getElementById('guppy');
@@ -78,11 +77,4 @@ $(document).ready()
     }
     bounce();
 
-
-    //cat go yippe when mouse hovers
-    $("#cat").hover(function(){
-        yippee.play();
-    });
-    
-    
     
